b8a994f1f5830d16e29e33cec26936faa6656fa8
Ahmad Hassan <ahmadhassantanveer44@gmail.com>
2024-11-14 01:05:49 +0500

    added article model and repo impl in data layer

---
+++ lib/features/daily_news/data/repos_impl/article_repo_impl.dart
@@ -0,0 +1,12 @@
+import 'package:news_app/core/resources/data_state.dart';
+import 'package:news_app/features/daily_news/data/models/article.dart';
+import 'package:news_app/features/daily_news/domain/entities/article.dart';
+import 'package:news_app/features/daily_news/domain/repos/article_repo.dart';
+
+class ArticleRepoImpl implements ArticleRepo{
+  @override
+  Future<DataState<List<ArticleModel>>> getArticles() {
+
+  }
+
+}
